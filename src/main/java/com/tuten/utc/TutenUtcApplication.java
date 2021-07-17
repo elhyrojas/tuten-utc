47818774b85d757ef78472cab96ad8bce06bfd07
elhyrojas <rojaselhy@gmail.com>
2021-07-16 20:35:39 -0400

    first commit

---
+++ src/main/java/com/tuten/utc/TutenUtcApplication.java
@@ -0,0 +1,13 @@
+package com.tuten.utc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutenUtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutenUtcApplication.class, args);
+	}
+
+}
